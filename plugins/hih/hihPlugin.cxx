c9dfaf0720dcde035fde27a7c6faca13f5acb831
mirabitl <pi@raspberrypi>
2021-03-10 17:09:54 +0100

    HIH8000 plugin

---
+++ plugins/hih/hihPlugin.cxx
@@ -0,0 +1,97 @@
+
+#include "hihPlugin.hh"
+#include <jsoncpp/json/json.h>
+using namespace lydaq;
+hihPlugin::hihPlugin(): _hih(NULL){} 
+void hihPlugin::open()
+{
+
+  LOG4CXX_INFO(_logPdaq," CMD: Open ");
+
+  std::cout<<"calling open "<<std::endl;
+  if (_hih!=NULL)
+    delete _hih;
+  
+ 
+  _hih= new lydaq::hih8000();
+
+}
+void hihPlugin::close()
+{
+  LOG4CXX_INFO(_logPdaq," CMD: closing Hih");
+  if (_hih==NULL)
+    {
+       LOG4CXX_ERROR(_logPdaq,"No HVHihInterface opened");
+       return;
+    }
+  
+  delete _hih;
+  _hih=NULL;
+}
+
+web::json::value hihPlugin::status()
+{
+  auto r=json::value::object();
+  r["name"]=json::value::string(U(this->hardware()));
+  r["status"]=json::value::string(U("UNKNOWN"));
+   if (_hih==NULL)
+    {
+      LOG4CXX_ERROR(_logPdaq,"No Hih Interface opened");
+       return r;
+    }
+   _hih->Read();
+   r["temperature0"]=json::value::number(_hih->temperature(0));
+   r["temperature1"]=json::value::number(_hih->temperature(1));
+   r["humidity0"]=json::value::number(_hih->humidity(0));
+   r["humidity1"]=json::value::number(_hih->humidity(1));
+
+   r["status"]=json::value::string(U("READ"));
+
+
+   return r;
+}
+
+
+void hihPlugin::c_status(http_request m)
+{
+  auto par = json::value::object();
+  if (_hih==NULL)
+  {
+    LOG4CXX_ERROR(_logPdaq,"No Hih opened");
+    par["status"]=json::value::string(U("No Device"));
+    Reply(status_codes::OK,par);
+
+    return;
+  }
+
+  par["status"] =this->status();
+  Reply(status_codes::OK,par);
+
+}
+
+
+void hihPlugin::registerCommands() 
+{
+
+ addCommand("HIH_STATUS",std::bind(&hihPlugin::c_status,this,std::placeholders::_1));
+
+   
+}
+
+
+
+extern "C" 
+{
+    // loadDHCALAnalyzer function creates new LowPassDHCALAnalyzer object and returns it.  
+  handlerPlugin* loadProcessor(void)
+    {
+      return (new hihPlugin);
+    }
+    // The deleteDHCALAnalyzer function deletes the LowPassDHCALAnalyzer that is passed 
+    // to it.  This isn't a very safe function, since there's no 
+    // way to ensure that the object provided is indeed a LowPassDHCALAnalyzer.
+  void deleteProcessor(handlerPlugin* obj)
+    {
+      delete obj;
+    }
+}
---
+++ plugins/hih/hihPlugin.hh
@@ -0,0 +1,38 @@
+#pragma once
+#include <iostream>
+
+#include <string.h>
+#include<stdio.h>
+#include "monitor.hh"
+#include "stdafx.hh"
+#include "hih8000.hh"
+
+using namespace std;
+#include <sstream>
+
+
+class hihPlugin : public monitoring::supervisor
+  {
+  public:
+    hihPlugin();
+    // Transition
+    virtual void open();
+    virtual void close();
+    virtual void registerCommands();
+    // Access to the interface
+    lydaq::hih8000* getHih8000Interface(){  //std::cout<<" get Ptr "<<_hih8000<<std::endl;
+      return _hih;}
+
+    // Status
+    virtual web::json::value status();
+    virtual std::string hardware(){return "HIH";}
+
+    
+    // Commande
+    void c_status(http_request m);
+
+
+  private:
+    lydaq::hih8000* _hih;
+  };
+
