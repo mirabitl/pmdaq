32b7dda5325c5f1765000638243d8a26315088f9
mirabitl <laurent.mirabito@cern.ch>
2021-03-13 08:04:33 +0100

    Logger tuning

--- include/pmLogger.hh
+++ include/pmLogger.hh
@@ -2,6 +2,7 @@
 
 #include <log4cxx/logger.h>
 #include <log4cxx/xml/domconfigurator.h>
+#include<string.h>
 using namespace log4cxx;
 using namespace log4cxx::xml;
 using namespace log4cxx::helpers;
@@ -10,8 +11,9 @@ static LoggerPtr _logPdaq(Logger::getLogger("PMDAQ"));
 static LoggerPtr _logPm(Logger::getLogger("PMDAQ_EVB"));
 static LoggerPtr _logPmex(Logger::getLogger("PMDAQ_EXAMPLE"));
 //static LoggerPtr _logZdaqex(Logger::getLogger("ZDAQEXAMPLE"));
-#define PM_DEBUG(a,b) LOG4CXX_DEBUG(a,__FILE__<<"("<<__LINE__<<")"<<b)
-#define PM_INFO(a,b) LOG4CXX_INFO(a,__FILE__<<"("<<__LINE__<<")"<<b)
-#define PM_WARN(a,b) LOG4CXX_WARN(a,__FILE__<<"("<<__LINE__<<")"<<b)
-#define PM_ERROR(a,b) LOG4CXX_ERROR(a,__FILE__<<"("<<__LINE__<<")"<<b)
-#define PM_FATAL(a,b) LOG4CXX_FATAL(a,__FILE__<<"("<<__LINE__<<")"<<b)
+#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+#define PM_DEBUG(a,b) LOG4CXX_DEBUG(a,__FILENAME__<<"("<<__LINE__<<")"<<b)
+#define PM_INFO(a,b) LOG4CXX_INFO(a,__FILENAME__<<"("<<__LINE__<<")"<<b)
+#define PM_WARN(a,b) LOG4CXX_WARN(a,__FILENAME__<<"("<<__LINE__<<")"<<b)
+#define PM_ERROR(a,b) LOG4CXX_ERROR(a,__FILENAME__<<"("<<__LINE__<<")"<<b)
+#define PM_FATAL(a,b) LOG4CXX_FATAL(a,__FILENAME__<<"("<<__LINE__<<")"<<b)
