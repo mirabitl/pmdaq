5348f661dbc9de431a44d630bdff5e534a183e04
mirabitl <laurent.mirabito@cern.ch>
2021-09-09 16:21:05 +0200

    MBMDCC closing debug

--- lyon/mbmdcc/interface.cc
+++ lyon/mbmdcc/interface.cc
@@ -27,7 +27,7 @@ void mbmdcc::Interface::dolisten()
     PM_INFO(_logMbmdcc,"Thread is "<<_running);
 
     if (!_running) break;
-    if(!_group->listen(4000,0))
+    if(!_group->listen())
       std::cout << "\nNo msg recieved during the last 4 seconds"<<std::endl<<std::flush;
   }
   PM_INFO(_logMbmdcc,"Thread is finished");
