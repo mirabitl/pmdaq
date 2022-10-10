8634be1f7295585906067949d6a15bf351477ff1
mirabitl <laurent.mirabito@cern.ch>
2022-10-10 13:44:28 +0200

    Debugged driver for 2 HIH read by raspberry

--- mqtt/hih/hihdriver.hh
+++ mqtt/hih/hihdriver.hh
@@ -17,8 +17,10 @@ extern "C"
 {
 
 #include <linux/types.h>
-  //#include <linux/i2c.h>
 #include <linux/i2c-dev.h>
+#include <linux/i2c.h>
+#include <i2c/smbus.h>
+
 }
 #include <sys/ioctl.h>
 
@@ -26,24 +28,17 @@ extern "C"
 //#include <i2c/smbus.h>
 #include <gpiod.h>
 
-#define TEMP 0x00
-#define CONFIG 0x01
-#define CONFIG 0x01
 #define DEV_ADDR 0x4A
 #define HUM_ADDR 0x27
 
-#define DELAY 5000000
+#define DELAY 2000000
 
 // GPIO stuff
 
-#define PINSELECT 4
-#define LOW 0
-#define HIGH 1
-#define DIR_IN 0
-#define DIR_OUT 1
-
+#define PINSELECT 23
+#define ExitOnFail 1
+#define DisplayFailMessage 1
 using namespace std;
-
 
 class hihdriver
 {
@@ -64,7 +59,6 @@ public:
     if (_fd > -1)
       close(_fd);
   }
-
   int Setup()
   {
     int temp1, temp2;
@@ -77,10 +71,10 @@ public:
 
     std::string fi2c = "/dev/i2c-1";
     if ((_fd = open(fi2c.c_str(), O_RDWR)) < 0)
-    {
-      std::cerr<< " unable to open I2C device " << fi2c << " _fd=" << _fd<<std::endl;
-      return -1;
-    }
+      {
+	std::cerr<< " unable to open I2C device " << fi2c << " _fd=" << _fd<<std::endl;
+	return -1;
+      }
 
     // First select the chip
 
@@ -90,63 +84,75 @@ public:
 
     int addr = HUM_ADDR; //<<<<<The I2C address of the slave
     if (ioctl(_fd, I2C_SLAVE, addr) < 0)
-    {
-      std::cerr<<" unable to open I2C at " << HUM_ADDR << " _fd=" << _fd<<std::endl; 
-      return -1;
-    }
+      {
+	std::cerr<<" unable to open I2C at " << HUM_ADDR << " _fd=" << _fd<<std::endl; 
+	return -1;
+      }
     else
       printf("fd=%d\n", _fd);
 
     return 0;
   }
-
   int chip_read(uint32_t chip)
   {
-    unsigned char buf[4]; /* Buffer for data read/written on the i2c bus */
+    
+    unsigned char buf[20]; /* Buffer for data read/written on the i2c bus */
     gpiod_line_set_value(_lineSel, chip);
-    ::usleep(100000);
-    buf[0] = HUM_ADDR;
-    write(_fd, buf, 1);
-    buf[0] = 0x00; // Read command
-    write(_fd, buf, 1);
-
-    if (read(_fd, buf, 4) < 0)
-    {
-      fprintf(stderr, "Unable to read from slave\n");
-      //    exit(1);
-      return -1;
-    }
+    ::usleep(1000000);
+    int val=gpiod_line_get_value(_lineSel);
+    printf(" Chip %d GPIO line value %d \n",chip,val);
+    //getchar();
+    buf[0]=HUM_ADDR;
+    //buf[0]=0;
+    //write(_fd,buf,1);
+     if ((i2c_smbus_write_quick(_fd, 0)) != 0)
+       {
+     	printf("Error writing bit to i2c slave\n");
+     	//exit(1);
+       }
+     ::usleep(400000);
+    int nr=read(_fd, buf, 4);
+    if (nr < 0)
+      {
+	fprintf(stderr, "Unable to read from slave %d\n",nr);
+	//    exit(1);
+	return -1;
+      }
     else
-    {
-      int reading_hum = (buf[0] << 8) + buf[1];
-      double humidity = reading_hum / 16382.0 * 100.0;
-      // printf("Humidity: %f\n", humidity);
+      {
+	printf("Bytes read %d \n",nr);
+	for (int k=0;k<nr;k++)
+	  printf("Buffer %d : %2x \n",k,buf[k]);
+	int reading_hum = (buf[0] << 8) + buf[1];
+	reading_hum=reading_hum&0x3FFF;
+	double humidity = reading_hum / 16382.0 * 100.0;
+	printf("Humidity %d: %f\n",chip, humidity);
 
-      int reading_temp = (buf[2] << 6) + (buf[3] >> 2);
-      double temperature = reading_temp / 16382.0 * 165.0 - 40;
-      printf("Temperature 1: %f %f\n", temperature, humidity);
-      _humidity[chip] = (humidity);
-      _temperature[chip] = (temperature) + 273.15;
-      return 0;
-    }
+	int reading_temp = (buf[2] << 6) + (buf[3] >> 2);
+	double temperature = reading_temp / 16382.0 * 165.0 - 40;
+	printf("Temperature %d: %f %f\n",chip, temperature, humidity);
+	_humidity[chip] = (humidity);
+	_temperature[chip] = (temperature) + 273.15;
+	return 0;
+      }
   }
-
   int Read()
   {
     int rc = chip_read(1);
-    if (rc != 0)
-      return rc;
+    //if (rc != 0)
+    //  return rc;
     rc = chip_read(0);
     return rc;
   }
 
   double humidity(int i) { return _humidity[i]; }
   double temperature(int i) { return _temperature[i]; }
-
 private:
   int _fd;
   double _humidity[2];
   double _temperature[2];
   struct gpiod_chip *_chip;
   struct gpiod_line *_lineSel;
+  int _handle;
 };
+
