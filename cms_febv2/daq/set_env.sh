a568208db72d8cd4587c9c36002b36ebd0e668f7
mirabitl <laurent.mirabito@cern.ch>
2023-05-14 11:59:50 +0200

    Add correct PATHs

--- cms_febv2/daq/set_env.sh
+++ cms_febv2/daq/set_env.sh
@@ -1,4 +1,7 @@
-export LD_LIBRARY_PATH=/opt/cactus/lib/:$LD_LIBRARY_PATH 
+export LD_LIBRARY_PATH=/opt/cactus/lib/:/opt/pmdaq/cms_febv2/daq:$LD_LIBRARY_PATH
+export DRVFEBV2=${HOME}/feb-backend-emulator/Python_project/
+export DAQFEBV2=/opt/pmdaq/cms_febv2
 export MGDBLOGIN=acqcmsmu/OpacIP2I@lyocmsmu06:27017@FEBROC
-export PYTHONPATH=${HOME}/febv2_access/:../:$PYTHONPATH 
-export PATH=.:${HOME}/febv2_access/:$PATH 
+export PYTHONPATH=${DAQFEBV2}:${DAQFEBV2}/db:${DAQFEBV2}/daq:${DRVFEBV2}:$PYTHONPATH
+#${HOME}/febv2_access/:../:$PYTHONPATH 
+export PATH=.:${DAQFEBV2}:${DAQFEBV2}/db:${DAQFEBV2}/daq:$PATH 
