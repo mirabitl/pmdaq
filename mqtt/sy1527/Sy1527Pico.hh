3e9a4b109cda892196ef8653fcb2ea2e0ad191ff
mirabitl <laurent.mirabito@cern.ch>
2024-03-06 10:00:09 +0100

    Update to handle discrete list of channels

--- mqtt/sy1527/Sy1527Pico.hh
+++ mqtt/sy1527/Sy1527Pico.hh
@@ -4,11 +4,13 @@
 #include <time.h>
 class Sy1527Pico : public PicoInterface {
 public:
-  Sy1527Pico(std::string id, std::string subid,std::string account,uint32_t first,uint32_t last);
+  Sy1527Pico(std::string id, std::string subid,std::string account,std::vector<uint32_t> valc);
   ~Sy1527Pico();
   void registerCommands();
   web::json::value channelStatus(uint32_t channel);
   web::json::value rangeStatus(int32_t first,int32_t last);
+  web::json::value validStatus();
+  std::vector<uint32_t> ch_list(web::json::value v);
   void opensocket();
   void closesocket();
   virtual void status(web::json::value v);
@@ -25,6 +27,6 @@ private:
   caen::HVCaenInterface* _hv;
   std::mutex _bsem;
   std::string _account,_host,_name,_pwd;
-  uint32_t _first,_last;
+  std::vector<uint32_t> _valid;
   time_t _last_connect;
 };
