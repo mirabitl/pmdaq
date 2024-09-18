5fbdc643ef8cb6879bb116ed0e33a3f18f6296cb
mirabitl <laurent.mirabito@cern.ch>
2024-09-18 11:07:25 +0200

    Start of doxygenation of mdcc pluggin

--- lyon/mdcc/FtdiUsbDriver.hh
+++ lyon/mdcc/FtdiUsbDriver.hh
@@ -1,4 +1,14 @@
 #pragma once
+/**
+ * @file FtdiUsbDriver.hh
+ * @author L.Mirabito
+ * @brief FTDI driver
+ * @version 1.0
+ * @date 2024-09-18
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 #include <iomanip>
 
 #include <time.h>
@@ -15,45 +25,247 @@
 #include <string>
 #include "stdafx.hh"
 #include "utils.hh"
+// Logger for FTDI functions
 static LoggerPtr _logFTDI(Logger::getLogger("PMDAQ_FTDI"));
-
+/**
+ * @brief Namespace for mdcc
+ * 
+ */
 namespace mdcc
 {
+  /**
+   * @brief Class to access FTDI USB chips
+   * 
+   */
   class FtdiUsbDriver {
   public:
-    enum RC {OK=0,TESTREG=1,CLOSE=2,PURGE=3,UNAVAILABLE=4,BULKERROR=5,TOOMANYTRIALS=6,TIMEOUT=7,EEPROMREAD=8,EEPROMWRITE=9,EEPROMERASE=10,RESETBUS=11,OPENFAILED=12};
+    /**
+     * @brief Construct a new Ftdi Usb Driver object
+     * 
+     * @param deviceIdentifier 
+     * @param productid 
+     */
     FtdiUsbDriver(char * deviceIdentifier ,uint32_t productid=0x6001);
+    /**
+     * @brief Destroy the Ftdi Usb Driver object
+     * 
+     */
     ~FtdiUsbDriver();
+    /**
+     * @brief Check the USB access
+     * 
+     * @param start Start register address
+     * @param count Length of register scans
+     */
     void checkReadWrite(uint32_t start,uint32_t count);
+    /**
+     * @brief Purge the FTDI FIFOs
+     * 
+     */
     void FT245Purge( void );
+    /**
+     * @brief Read 1 byte 
+     * 
+     * @verbatim 
+     * RC 
+     * -666 No USB available
+     * <0   USB bulk erro
+     * 0   Too many tries
+     * 1 Ok
+     * @endverbatim
+     * 
+     * @param resultPtr Destination buffer
+     * @return int32_t Number of bytes or error code
+     */
     int32_t read( unsigned char  *resultPtr );
+    /**
+     * @brief Read N bytes 
+     * 
+     * @param resultPtr Destination buffer
+     * @param nbbytes Number of bytes to read
+     */
     void readNb( unsigned char  *resultPtr,int32_t nbbytes );
+    /**
+     * @brief Write 1 byte 
+     * 
+     * @param data Byte to write
+     */
     void write( unsigned char  data);
+    /**
+     * @brief Write N bytes
+     * 
+     * @param cdata source buffer
+     * @param nb Number of bytes to write
+     */
     void writeNb( unsigned char  *cdata, uint32_t nb);
+    /**
+     * @brief Read  one short fromEEPROM
+     * 
+     * @param address Address to read
+     * @param resultPtr destination buffer
+     */
     void readEEPROM( uint32_t address, 	uint32_t *resultPtr );
+    /**
+     * @brief Write data to the eeprom
+     * 
+     * @param address Eeprom address
+     * @param data data to write
+     */
     void writeEEPROM( 	uint32_t address,	uint32_t data);
+    /**
+     * @brief Reset EEPROM
+     * 
+     */
     void resetEEPROM( 	void);
+    /**
+     * @brief Reset USB bus
+     * 
+     */
     void resetBus( );
+    /**
+     * @brief Read FTDI status
+     * 
+     * @param RXQueue 
+     * @param TXQueue 
+     * @param Event 
+     * @deprecated Always 1 in RxQueue
+     */
     void readStatus( uint32_t *RXQueue, uint32_t *TXQueue, uint32_t *Event);
+    /**
+     * @brief Read MDCC version at address 0x100
+     * 
+     * @param version destination address
+     * @return int32_t error code -1 if read error
+     */
     int32_t UsbGetFirmwareRevision(uint32_t *version) ;
+    /**
+     * @brief Set the Test register value at address 0x2
+     * 
+     * @param tvalue value to write
+     * @return int32_t error code -2 if read error
+     */
     int32_t SetTestRegister(int32_t tvalue)   ;
+    /**
+     * @brief Get the Test Register value
+     * 
+     * @param tvalue destination buffer
+     * @return int32_t error code -2 if read error
+     */
     int32_t GetTestRegister(uint32_t *tvalue)   ;
+    /**
+     * @brief Read one byte
+     * 
+     * @param tbyte destination buffer
+     * @return int32_t 1 or error 0
+     */
     int32_t UsbReadByte(unsigned char  *tbyte);
+    /**
+     * @brief Read 4 bytes
+     * 
+     * @param data destination buffer
+     * @return int32_t 4 or error 0
+     */
     int32_t UsbRead4Bytes(uint32_t *data);
+    /**
+     * @brief Read 16 bytes
+     * 
+     * @param data destination buffer
+     * @return int32_t 16 or error 0
+     */
     int32_t UsbRead16Bytes(unsigned char  *data);
+    /**
+     * @brief Read 22 bytes
+     * 
+     * @param data destination buffer
+     * @return int32_t 22 or error 0
+     */
     int32_t UsbRead22Bytes(unsigned char  *data);
+    /**
+     * @brief Read N bytes
+     * 
+     * @param data destination buffer
+     * @param nbbytes Number of bytes to read
+     * @return int32_t nbbytes or error 0
+     */
     int32_t UsbReadnBytes(unsigned char  *data, int32_t nbbytes);
+    /**
+     * @brief Read 3 bytes
+     * 
+     * @param data destination buffer
+     * @return int32_t 3 or error 0
+     */
     int32_t UsbRead3Bytes(uint32_t *data);
+    /**
+     * @brief Read 2 bytes
+     * 
+     * @param data destination buffer
+     * @return int32_t 2 or error 0
+     */
     int32_t UsbRead2Bytes(uint32_t *data);
+    /**
+     * @brief read register value (32 bits) at address
+     * 
+     * @param address register address
+     * @param data Destination buffer
+     * @return int32_t 0 Ok negative error
+     */
     int32_t UsbRegisterRead(uint32_t address, uint32_t *data);
+    /**
+     * @brief Write data in a register
+     * 
+     * @param address Register address
+     * @param data Data to write
+     * @return int32_t 0 Ok negative error
+     * #deprecated proxy to UsbRegisterWrite2
+     */
     int32_t UsbRegisterWrite(uint32_t address, uint32_t data);
+    /**
+     * @brief Write data in a register
+     * 
+     * @param address Register address
+     * @param data Data to write
+     * @return int32_t 0 Ok negative error
+     */
     int32_t UsbRegisterWrite2(uint32_t address, uint32_t data);
+    /**
+     * @brief Write a MDCC command
+     * 
+     * @param command Command value
+     * @return int32_t 0 Ok negative error
+     */
     int32_t UsbCommandWrite(uint32_t command);
+    /**
+     * @brief Get FTDI status
+     * 
+     * @param RXQueue 
+     * @param TXQueue 
+     * @param Event 
+     * @return int32_t 0 and RxQueue set to 1
+     * @deprecated Allways 1 in RxQueue
+     */
     int32_t  FT245GetStatus(int32_t *RXQueue,int32_t *TXQueue ,int32_t *Event)   ;
+    /**
+     * @brief Reset the bus
+     * 
+     * @return int32_t 0 Ok negative error
+     */
     int32_t FT245Reset(void);
+    /**
+     * @brief Encode error message from return code
+     * 
+     * @return std::string 
+     */
     std::string rcMessage();
+    /**
+     * @brief Check good return code
+     * 
+     * @return true 
+     * @return false 
+     */
     bool isOk(){return _rc==RC::OK;}
     static uint16_t CrcTable[256];
+    enum RC {OK=0,TESTREG=1,CLOSE=2,PURGE=3,UNAVAILABLE=4,BULKERROR=5,TOOMANYTRIALS=6,TIMEOUT=7,EEPROMREAD=8,EEPROMWRITE=9,EEPROMERASE=10,RESETBUS=11,OPENFAILED=12};
+
   protected:
     struct ftdi_context theFtdi;
     uint32_t timeOut;
--- lyon/mdcc/MdccManager.hh
+++ lyon/mdcc/MdccManager.hh
@@ -1,4 +1,14 @@
 #pragma once
+/**
+ * @file MdccManager.hh
+ * @author L.Mirabito
+ * @brief MDCC Main application
+ * @version 1.0
+ * @date 2024-09-18
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 #include <iostream>
 #include <string.h>
 #include<stdio.h>
@@ -9,34 +19,163 @@ using namespace std;
 #include <sstream>
 #include "stdafx.hh"
 using namespace mdcc;
+/**
+ * @brief fsmw plugin to handle MDCC
+ * 
+ */
 class MdccManager : public fsmw
   {
   public:
+  /**
+   * @brief Construct a new Mdcc Manager object
+   * 
+   */
     MdccManager();
+    /**
+     * @brief Implementation on fsmw initialise
+     * 
+     * define states, transition and commands
+     * 
+     */
     virtual void initialise();
+    /**
+     * @brief Cleanly stop the plugin
+     * 
+     */
     virtual void end();
     
+    /**
+     * @brief INITIALISE transition
+     * 
+     * @param m The http request containing the transition command, parameters and answer
+     */
     void fsm_initialise(http_request m);
+    /**
+     * @brief DESTROY transition
+     * 
+     * @param m The http request containing the transition command, parameters and answer
+     */
     void destroy(http_request m);
 
-    
+    /**
+     * @brief Open the FtdiUsbDriver via the MdccHandler
+     * 
+     * @param s Ftdi device name
+     */
     void doOpen(std::string s);
+    /**
+     * @brief Get the Mdcc Handler object
+     * 
+     * @return MdccHandler* pointer to the handler
+     */
     MdccHandler* getMdccHandler(){  return _mdcc;}
 
+    /**
+     * @brief call back of STATUS command
+     * 
+     * @param m http request
+     */
     void c_status(http_request m);
+     /**
+     * @brief call back of PAUSE command
+     * 
+     * @param m http request
+     */
     void c_pause(http_request m);
+     /**
+     * @brief call back of RESUME command
+     * 
+     * @param m http request
+     */
     void c_resume(http_request m);
+     /**
+     * @brief call back of ECALPAUSE command
+     * 
+     * @param m http request
+     */
     void c_ecalpause(http_request m);
+     /**
+     * @brief call back of ECALRESUME command
+     * 
+     * @param m http request
+     */
     void c_ecalresume(http_request m);
+     /**
+     * @brief call back of RESET (counters) command
+     * 
+     * @param m http request
+     */
     void c_reset(http_request m);
+     /**
+     * @brief call back of READREG command
+     * 
+     * One parameter in the request, \b address is required 
+     * 
+     * @param m http request
+     */
     void c_readreg(http_request m);
+    /**
+     * @brief call back of READREG command
+     * 
+     * Two parameters are required in the request,
+     * 
+     *  \b address  and \b value 
+     * 
+     * @param m http request
+     */
     void c_writereg(http_request m);
+    /**
+     * @brief call back of SPILLON command
+     * 
+     * One parameter in the request, \b nclock is required 
+     * 
+     * @param m http request
+     */
     void c_spillon(http_request m);
+    /**
+     * @brief call back of SPILLOFF command
+     * 
+     * One parameter in the request, \b nclock is required 
+     * 
+     * @param m http request
+     */
     void c_spilloff(http_request m);
+    /**
+     * @brief call back of BEAMON command
+     * 
+     * One parameter in the request, \b nclock is required 
+     * 
+     * @param m http request
+     * @deprecated Not implemmneted in firmware anymore
+     */
     void c_beamon(http_request m);
+    /**
+     * @brief call back of SETCALIBCOUNT command
+     * 
+     * One parameter in the request, \b nclock is required 
+     * 
+     * It is the number of windows per calibration point
+     * 
+     * @param m http request
+     */
     void c_setcalibcount(http_request m);
+    /**
+     * @brief call back of RELOADCALIB command
+     *      
+     * @param m http request
+     */
     void c_reloadcalib(http_request m);
+    /**
+     * @brief call back of CALIBON command
+     *      
+     * @param m http request
+     */
     void c_calibon(http_request m);
+    /**
+     * @brief call back of CALIBOFF command
+     *     
+     * @param m http request
+     */
     void c_caliboff(http_request m);
     void c_resettdc(http_request m);
     void c_setcalibregister(http_request m);
@@ -47,6 +186,7 @@ class MdccManager : public fsmw
     void c_getregister(http_request m);
     void c_setexternaltrigger(http_request m);
   private:
+    /// @brief Pointer to the mdcc::MdccHandler
     mdcc::MdccHandler* _mdcc;
 };
 
