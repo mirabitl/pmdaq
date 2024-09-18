c1b1ed57baa1ece0b49ee3d17e3b91575933188a
mirabitl <laurent.mirabito@cern.ch>
2024-09-18 17:31:47 +0200

    Documentation

--- lyon/mdcc/MdccManager.hh
+++ lyon/mdcc/MdccManager.hh
@@ -20,7 +20,51 @@ using namespace std;
 #include "stdafx.hh"
 using namespace mdcc;
 /**
- * @brief fsmw plugin to handle MDCC
+ * 
+ *@brief fsmw plugin to handle MDCC
+ * 
+ * The register accessible in the firmware are described in mdcc::MdccHandler
+ * 
+ * 
+ * The Finite State Machine is defined as 
+ * 
+ * |States|
+ * |------|
+ * | CREATED|
+ * | OPENED|
+ * 
+ * With the following transitions
+ * 
+ * |transitions| transition command|
+ * |----------:|:------------------|
+ * CREATED-OPENED| INITIALISE 
+ * OPENED-CREATED| DESTROY
+ * 
+ * And the following commands are implemnted
+ * 
+ * | Command | Action| 
+ * |--------:|:------|
+ * | PAUSE | \b Software_veto=1 |
+ * | RESUME | \b Software_veto=0 |
+ * |RESET | Reset the counters Flip \b  Control=1 and then 0| 
+ *ECALPAUSE | Obsolete 
+ * ECALRESUME | Obsolete 
+ * WRITEREG/SETREG | Write one register with \e  address (decimal) and \e  value parameters 
+ * READREG/GETREG | Read value of one register at \e  address (decimal) parameter 
+ * STATUS | Read and returns the value of all parameters in a JSON object 
+ * SPILLON | Set \b  spillon  to \e  nclock parameter value 
+ * SPILLOFF | Set \b  spilloff  to \e  nclock parameter value 
+ * BEAMON | Set \b  beam  to \e  nclock parameter value (obsolete) 
+ * RESETTDC/SETHARDRESET | Set \b  Rstdet to \e  value parameter (should be flip 1 to 0) 
+ * CALIBON | Set \b  Calib  to 2 
+ * CALIBOFF | Set \b  Calib  to 0 
+ * RELOADCALIB | Do the full MDCC \e  per setting point sequence previously described 
+ * SETCALIBCOUNT | Set \b  nb_windows  to \e  nclock parameter value 
+ * SETSPILLREGISTER | Set \b  WindowConfig  to \e  value parameter 
+ * SETCALIBREGISTER | Set \b  Calib to \e  value parameter 
+ * SETTRIGEXT | Set \b  TrigExtDelay to \e  delay value and \b  TrigExtLength to \e  busy value 
+ * SETEXTERNAL | \b  Enable_busy_on_trigger set to \e  value (0 or 1) parameter 
+ *  
  * 
  */
 class MdccManager : public fsmw
@@ -35,6 +79,8 @@ class MdccManager : public fsmw
      * @brief Implementation on fsmw initialise
      * 
      * define states, transition and commands
+     * 
+     *
      * 
      */
     virtual void initialise();
